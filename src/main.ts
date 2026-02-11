c900b56d9e027f8c6773361548f8ad1820f5a494
Joel-D-Rodriguez <joerh0803@gmail.com>
2026-02-11 15:15:56 -0400

    Fix: Listen on 0.0.0.0 to fix Railway 502 Gateway error

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. Habilitar CORS (¡CRUCIAL!)
-  // Esto permite que el puerto 5173 (Frontend) hable con el 3000 (Backend)
-  app.enableCors();
+  // 1. Habilitar CORS
+  // Permite solicitudes desde cualquier origen (útil para que Vercel no sea bloqueado)
+  app.enableCors({
+    origin: '*', // Permite solicitudes desde cualquier origen//
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // 2. Agregar prefijo global
-  // Esto hace que todas tus rutas empiecen con /api (ej: /api/products)
+  // Tus rutas quedarán como: https://.../api/products
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  // 3. Configuración del Puerto y Host (CRUCIAL PARA RAILWAY)
+  // El '0.0.0.0' es obligatorio en contenedores Docker/Railway para evitar el error 502/503
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
